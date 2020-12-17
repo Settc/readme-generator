84a38b1a3dc73fedf0aa2a94cf3421f652e95892
Thomas Whittle <tewhittle@gmail.com>
2020-12-17 14:05:15 -0500

    Improve markdown template in index.js, add video guide to README

--- README.md
+++ README.md
@@ -2,11 +2,47 @@
 # Readme Generator
 
 ## Description
-
+![License](https://img.shields.io/badge/License-MIT-blue.svg)
+    A node.js readme generator, using command line prompts to fill out a template in order to speed up the readme writing process for your projects. The readme you're currently reading was made using 
+    the generator to give you an idea of what your finished readme will look like.
 ## Table of Contents
+
 * [Installation](#Installation)
 * [Usage](#Usage)
 * [Contributing](#Contributing)
 * [Tests](#Tests)
 * [Questions](#Questions)
 * [License](#License)
+
+### Installation:  
+  
+    npm init 
+
+to create a package.json file. 
+
+    npm i inquirer 
+
+to install the required module. 
+    
+    node index.js 
+to begin the prompts.
+
+## [Video Guide](https://www.youtube.com/watch?v=idAeU3kmWf0&feature=youtu.be)
+
+### Usage :
+Simply fill out the prompts, the program will do the rest.
+
+### Contributing :
+Me, Thomas Whittle (full stack developer for hire)
+
+### Tests :
+none
+
+### Questions? :
+### www.github.com/settc  
+### tewhittle@gmail.com
+
+### License: 
+    This work is protected by the MIT license. 
+    Please go to 
+    www.opensource.org/licenses to learn more
--- index.js
+++ index.js
@@ -61,50 +61,6 @@ const questions = () => {
     ])
 }
 
-const generateMD = (answers) => 
-`
-# ${answers.title}
-
-## Description
-[![License](https://img.shields.io/badge/License-${answers.license}-blue.svg)]
-    ${answers.description}
-## Table of Contents
-
-* [Installation](#Installation)
-* [Usage](#Usage)
-* [Contributing](#Contributing)
-* [Tests](#Tests)
-* [Questions](#Questions)
-* [License](#License)
-
-### Installation:
-    ${answers.installation}
-
-### Usage:
-    ${answers.usage}
-
-### Contributing:
-    ${answers.contributing}
-
-### Tests:
-    ${answers.test}
-
-### Questions?:
-    www.github.com/${answers.github}
-    ${answers.email}
-
-### License: This work is protect by the ${answers.license} license. Please go to 
-    www.opensource.org/licenses to learn more
-`
-
-
-// Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// function to write README file
-// function writeToFile(fileName, data) {
-//     fs.writeFile()
-// }
-
-// function to initialize program
 const init = async () => {
     try {
         const answers = await questions()
@@ -118,22 +74,42 @@ const init = async () => {
 
 }
 
-// function call to initialize program
+const generateMD = (answers) => 
+
+`
+# ${answers.title}
+
+## Description
+[!License](https://img.shields.io/badge/License-${answers.license}-blue.svg)
+    ${answers.description}
+## Table of Contents
+
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Contributing](#Contributing)
+* [Tests](#Tests)
+* [Questions](#Questions)
+* [License](#License)
+
+### Installation :
+${answers.installation}
+
+### Usage :
+${answers.usage}
+
+### Contributing :
+${answers.contributing}
+
+### Tests :
+${answers.test}
+
+### Questions? :
+### www.github.com/${answers.github}
+### ${answers.email}
+
+### License : 
+This work is protected by the ${answers.license} license. Please go to 
+www.opensource.org/licenses to learn more
+`
+
 init();
-
-
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
